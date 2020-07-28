f9c9da54b936f925bf253ce61f2edab078881099
Andreas <ahindman@uw.edu>
2020-07-27 23:13:59 -0700

    added target generation and clicking functionality

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
-console.log("timer code goes here");
+var body = document.getElementsByTagName("BODY")[0];
+
+setInterval(addRandomTarget, 1000);
+
+body.onclick = function(event) {
+    console.log("Clicked", event.target);
+    event.target.remove();
+}
+
+function addRandomTarget() {
+    var posFromLeft = (getRndDouble(0, innerWidth - 100));
+    var posFromTop = (getRndDouble(0, innerHeight - 100));
+    body.innerHTML += `<svg class="target-container" width="100" height="100" style="left: ${posFromLeft}px; top: ${posFromTop}px;"> <circle class="target" cx="50" cy="50" r="40"/> </svg>`
+}
+
+function getRndDouble(min, max) {
+    return (Math.random() * (max - min)) + min
+}
+
+// var btn = document.createElement("BUTTON");
+// btn.innerHTML = "CLICK ME";
+// document.body.appendChild(btn);
